f52d117bff010a55bceeffb17517700d81ee5da1
gelisenyazilimci <burbytes@gmai.com>
2024-12-14 07:17:16 +0300

    Day-3

---
+++ Day-3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day_3;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine()!.Trim());
+        string result = (n % 2 != 0 || (n % 2 == 0 && n >= 6 && n <= 20)) ? "Weird" : "Not Weird";
+        Console.WriteLine(result);
+    }
+}
