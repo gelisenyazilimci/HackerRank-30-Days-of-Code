bae1503d4e9d2c1414d25bafbf2eb7e501d97b48
gelisenyazilimci <burbytes@gmai.com>
2024-12-14 14:25:41 +0300

    Day-7

---
+++ Day-7/Day-7/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day_7;
+internal static class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arrTemp = Console.ReadLine()!.Split(' ');
+        int[] arr = Array.ConvertAll(arrTemp,int.Parse);
+        Array.Reverse(arr);
+        foreach(var i in arr) Console.Write(i+" ");
+    }
+}
