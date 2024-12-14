49f80bbdf03f78e67253548d62290baa8da458c4
gelisenyazilimci <burbytes@gmai.com>
2024-12-14 07:39:29 +0300

    Day-5

---
+++ Day-5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day_5;
+
+internal static class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine()!.Trim());
+        if (n <= 20 && n >= 2)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} x {i} = {n * i}");   
+            }
+        }
+    }
+}
